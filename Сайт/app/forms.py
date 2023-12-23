c8bb7f2f891582fe70752003967217a16c3ace71
4217 Mostovoy A.A. <sasabaranov189@gmail.com>
2023-12-23 16:07:28 +0300

    Исправлена ошибка ввода данных

--- Сайт/app/forms.py
+++ Сайт/app/forms.py
@@ -3,16 +3,17 @@ Definition of forms.
 """
 
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
 
-class BootstrapAuthenticationForm(AuthenticationForm):
-    """Authentication form which uses boostrap CSS."""
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder':'Password'}))
+class ExamScoresForm(forms.Form):  
+    russian = forms.IntegerField(min_value=0, max_value = 100, label='Russian', required= False)
+    physics = forms.IntegerField(min_value=0, max_value = 100,label='Physics', required= False)
+    literature = forms.IntegerField(min_value=0, max_value = 100,label='Literature', required= False)
+    history = forms.IntegerField(min_value=0, max_value = 100,label='History', required= False)
+    math = forms.IntegerField(min_value=0, max_value = 100,label='Math', required= False)
+    chemistry = forms.IntegerField(min_value=0, max_value = 100,label='Chemistry', required= False)
+    geography = forms.IntegerField(min_value=0, max_value = 100,label='Geography', required= False)
+    socialstudies = forms.IntegerField(min_value=0, max_value = 100,label='Socialstudies', required= False)
+    informatics = forms.IntegerField(min_value=0, max_value = 100,label='Informatics', required= False)
+    biology = forms.IntegerField(min_value=0, max_value = 100,label='Biology', required= False)
+    english = forms.IntegerField(min_value=0, max_value = 100, label='English', required= False)
